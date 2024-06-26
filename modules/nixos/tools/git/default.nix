f79eb65751f2ddc63441844a018233b09c38c346
James Duff <jaduff@protonmail.com>
2024-06-26 17:25:22 +0800

    switch to correct public key

--- modules/nixos/tools/git/default.nix
+++ modules/nixos/tools/git/default.nix
@@ -17,7 +17,7 @@ in {
     userName = mkOpt types.str user.fullName "The name to configure git with.";
     userEmail = mkOpt types.str user.email "The email to configure git with.";
     signingKey =
-      mkOpt types.str "9762169A1B35EA68" "The key ID to sign commits with.";
+      mkOpt types.str "345C37618987B16" "The key ID to sign commits with.";
   };
 
   config = mkIf cfg.enable {
