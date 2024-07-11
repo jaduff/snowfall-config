4eb4221bb66effd02f35f1cf8411dc54e1620772
James Duff <jaduff@protonmail.com>
2024-07-11 20:02:31 +0800

    added lix cache

--- modules/nixos/cache/public/default.nix
+++ modules/nixos/cache/public/default.nix
@@ -19,6 +19,8 @@ in
 #      "https://attic.ruby.hamho.me/public".key = "public:QUkZTErD8fx9HQ64kuuEUZHO9tXNzws7chV8qy/KLUk=";
       "https://cache.nixos.org".key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
       "https://nix-community.cachix.org".key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+      "https://cache.lix.systems" = "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o=";
+
     };
   };
 }
