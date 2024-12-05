67f2d7b8b3ab00c26fec389f5c4cc1de25a4562b
James Duff <jaduff@protonmail.com>
2024-12-05 20:07:23 +0800

    added whatsapp-for-linux

---
+++ modules/nixos/apps/whatsapp-for-linux/default.nix
@@ -0,0 +1,20 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace}; let
+  cfg = config.${namespace}.apps.whatsapp-for-linux;
+in {
+  options.${namespace}.apps.whatsapp-for-linux = with types; {
+    enable = mkBoolOpt false "Whether or not to enable Protontricks.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [whatsapp-for-linux];
+  };
+}
