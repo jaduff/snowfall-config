3fc4b261686f16aa8439af5041c93e9f8075052b
James Duff <jaduff@protonmail.com>
2025-02-09 11:47:08 +0800

    added hp to printing

--- modules/nixos/services/printing/default.nix
+++ modules/nixos/services/printing/default.nix
@@ -16,5 +16,27 @@ in
     enable = mkBoolOpt false "Whether or not to configure printing support.";
   };
 
-  config = mkIf cfg.enable { services.printing.enable = true; };
+  config = mkIf cfg.enable {
+    services.printing.enable = true;
+    environment.systemPackages = with pkgs; [
+	gutenprint
+	fflinuxprint
+	fxlinuxprint
+	cups-kyodialog
+	gutenprintBin
+	hplip
+    ];
+    services.printing.drivers = with pkgs; [
+	foomatic-db-ppds-withNonfreeDb
+	fflinuxprint
+	gutenprint
+	hplip
+	cups-pdf-to-pdf
+	gutenprint
+	fflinuxprint
+	fxlinuxprint
+	cups-kyodialog
+	gutenprintBin
+    ];
+  };
 }
