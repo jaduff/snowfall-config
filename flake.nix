e2e1532117e02cce53d989deb0c341f167eaafb7
James Duff <jaduff@protonmail.com>
2025-02-10 17:03:09 +0800

    added veracrypt overlay

--- flake.nix
+++ flake.nix
@@ -45,6 +45,9 @@
     comma.url = "github:nix-community/comma";
     comma.inputs.nixpkgs.follows = "unstable";
 
+    # Veracrypt
+    veracrypt.url = "github:NixOs/nixpkgs/4054e4f776b62f59acafce83d6c30a839f913fc7";
+    #veracrypt.inputs.nixpkgs.follows = "unstable";
 
     # Tmux
     tmux.url = "github:jakehamilton/tmux";
