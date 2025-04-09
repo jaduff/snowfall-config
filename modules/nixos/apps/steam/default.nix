a9d3f872ff062686883e81bf5de61a62741fccb0
James Duff <jaduff@protonmail.com>
2025-04-09 18:38:38 +0800

    added firewall rules

--- modules/nixos/apps/steam/default.nix
+++ modules/nixos/apps/steam/default.nix
@@ -19,10 +19,10 @@ in
   config = mkIf cfg.enable {
     programs.steam.enable = true;
     programs.steam.remotePlay.openFirewall = true;
-    networking.firewall.allowedTCPPorts = [27040 6073];
+    networking.firewall.allowedTCPPorts = [27040 6073 27015 27036];
     networking.firewall.allowedTCPPortRanges = [{from=2300;to=2400;}];
-    networking.firewall.allowedUDPPorts = [47630 6073];
-    networking.firewall.allowedUDPPortRanges = [{ from=27031; to=27036; } {from=2300; to=2400;}];
+    networking.firewall.allowedUDPPorts = [47630 6073 33540];
+    networking.firewall.allowedUDPPortRanges = [{ from=27031; to=27036; } {from=2300; to=2400;} {from=25015;to=27016;} {from=27031;to=27036;}];
     hardware.steam-hardware.enable = true;
 
     # Enable GameCube controller support.
