9ed89f1bf7911faa03c0b552c201d4c80733e86f
James Duff <jaduff@protonmail.com>
2025-12-17 17:23:59 +0800

    modified wireguard keys

--- systems/x86_64-linux/ignorant/default.nix
+++ systems/x86_64-linux/ignorant/default.nix
@@ -58,7 +58,7 @@ virtualisation.libvirtd.enable = true;
     declarative = true;
     serverProperties = {
       server-port = 43000;
-      level-seed = "bananas-jellyfish";
+      #level-seed = "bananas-jellyfish";
     };
   };
 
