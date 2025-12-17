9d371caf212143cc9a839a0958dddb0b4ed8f40a
James Duff <jaduff@protonmail.com>
2025-12-17 21:15:54 +0800

    wireguard config now working

--- modules/nixos/services/wireguard/dingopaw/default.nix
+++ modules/nixos/services/wireguard/dingopaw/default.nix
@@ -53,7 +53,7 @@ in {
     };
      # enable NAT
   networking.nat.enable = true;
-  networking.nat.externalInterface = "eth0";
+  networking.nat.externalInterface = "end0";
   networking.nat.internalInterfaces = [ "wg0" ];
   networking.firewall = {
     allowedUDPPorts = [ 51820 ];
